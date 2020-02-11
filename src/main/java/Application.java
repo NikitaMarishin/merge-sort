7259a905b8fb7921348a7ceadbb0b1596fc6b2c7
hiroo <n.s.marishin@gmail.com>
2020-02-11 11:25:44 +0700

    rename Main

--- src/main/java/Application.java
+++ src/main/java/Application.java
@@ -2,7 +2,7 @@ import analyzer.Analyzer;
 import analyzer.AnalyzerImp;
 
 
-public class Main {
+public class Application {
     public static void main(String[] args) {
         Analyzer analyzer = new AnalyzerImp();
         if (analyzer.analyze(args)) {
